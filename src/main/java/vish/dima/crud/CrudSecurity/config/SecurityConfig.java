9ecc9a1dbd51ff9d9768b706ad455c46acb9df65
Dima Vishnyakov <dmitriivishnyakov2003@gmail.com>
2024-10-31 02:38:59 +0400

    delete AuthProviderImpl and add new SecurityConfig

--- src/main/java/vish/dima/crud/CrudSecurity/config/SecurityConfig.java
+++ src/main/java/vish/dima/crud/CrudSecurity/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package vish.dima.crud.CrudSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import vish.dima.crud.CrudSecurity.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import vish.dima.crud.CrudSecurity.services.PersonDetailsService;
 
 
 @EnableWebSecurity
+@Component
 public class SecurityConfig {
-    private final AuthProviderImpl authProvider;
 
+    private final PersonDetailsService personDetailsService;
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
     }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
