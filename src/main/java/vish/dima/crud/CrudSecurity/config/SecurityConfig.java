b3bc5980720c50c7cfae3f99491b5258dd91d102
Dima Vishnyakov <dmitriivishnyakov2003@gmail.com>
2024-11-01 02:17:27 +0400

    Адаптация старого кода, обновление конфига с учетом Spring 6

--- src/main/java/vish/dima/crud/CrudSecurity/config/SecurityConfig.java
+++ src/main/java/vish/dima/crud/CrudSecurity/config/SecurityConfig.java
@@ -2,31 +2,57 @@ package vish.dima.crud.CrudSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
 import vish.dima.crud.CrudSecurity.services.PersonDetailsService;
 
-
 @EnableWebSecurity
-@Component
+@Configuration
 public class SecurityConfig {
 
     private final PersonDetailsService personDetailsService;
+
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
+        return authenticationManagerBuilder.build();
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/login", "/error").permitAll() // Разрешить доступ к странице логина
+                        .anyRequest().authenticated() // Защищаем все другие запросы
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error")
+                );
+
+        return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // Убедитесь, что это использование только для тестирования.
+    }
 }
