c1eccd397314d42eebcc68e23e1a963b72427a34
Dima Vishnyakov <dmitriivishnyakov2003@gmail.com>
2024-11-01 19:41:43 +0400

    Добавление шифрования паролей

--- src/main/java/vish/dima/crud/CrudSecurity/config/SecurityConfig.java
+++ src/main/java/vish/dima/crud/CrudSecurity/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,7 +28,8 @@ public class SecurityConfig {
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(personDetailsService);
+        authenticationManagerBuilder.userDetailsService(personDetailsService)
+                .passwordEncoder(passwordEncoder());
         return authenticationManagerBuilder.build();
     }
 
@@ -55,6 +57,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance(); // Убедитесь, что это использование только для тестирования.
+       return new BCryptPasswordEncoder(); // Шифрование
     }
 }
--- src/main/java/vish/dima/crud/CrudSecurity/services/RegistrationService.java
+++ src/main/java/vish/dima/crud/CrudSecurity/services/RegistrationService.java
@@ -1,21 +1,29 @@
 package vish.dima.crud.CrudSecurity.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import vish.dima.crud.CrudSecurity.models.Person;
 import vish.dima.crud.CrudSecurity.repositories.PeopleRepository;
+
 @Service
 public class RegistrationService {
 
+    private final PasswordEncoder passwordEncoder;
     private final PeopleRepository peopleRepository;
+
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PasswordEncoder passwordEncoder, PeopleRepository peopleRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.peopleRepository = peopleRepository;
     }
 
     @Transactional
     public void register(Person person) {
-    peopleRepository.save(person);
+
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+
+        peopleRepository.save(person);
     }
 }
